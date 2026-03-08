a818873db4d012043d2152218e2011a7d9c543af
苏洋 <1493367306@qq.com>
2026-03-09 01:49:55 +0800

    Fix SITE_POLICE_LINK URL in config
    Updated SITE_POLICE_LINK to use the correct URL.

--- src/config.ts
+++ src/config.ts
@@ -64,8 +64,8 @@ export const SITE_ICP = config.site.icp ?? "";
 export const SITE_ICP_LINK = config.site.icp_link ?? "https://beian.miit.gov.cn/";
 
 // 公安备案号
-export const SITE_POLICE = config.site.police ?? "";
-export const SITE_POLICE_LINK = config.site.police_link ?? "https://beian.mps.gov.cn/";
+export const SITE_ICP = config.site.icp ?? "";
+export const SITE_ICP_LINK = config.site.icp_link ?? "https://beian.mps.gov.cn/";
 
 // 多语言文本配置
 export const TRANSLATIONS = translationsConfig;
